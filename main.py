1ef1cf43b6feb694bdb5449bd8a27be8cb7ef2b0
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-02 10:28:00 -0300

    test

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
 import os
 
-API_ORIGEM = 'https://busca.maplebear.com.br/api/sn/maplebear-prd-publish/search?p=1&sort=publication_date:desc&rows=240&_setlocale=pt_BR&nfpr=0&fq[]=mbtype:manual&q='
+API_ORIGEM =
 API_DESTINO = 'https://api.destino.com/receber'
 
 HEADERS_DESTINO = {
     'Content-Type': 'application/json'
 }
 
-def getManuals():
+def getAllTuring(page):
     try:
-        resposta = requests.get(API_ORIGEM, verify=False)
+        resposta = requests.get('https://busca.maplebear.com.br/api/sn/maplebear-prd-publish/search?p='+ page +'&rows=100&_setlocale=pt_BR&nfpr=0&q=', verify=False)
         resposta.raise_for_status()
         dados = resposta.json()
         return dados
@@ -27,11 +27,15 @@ def enviar_dados(dados):
         print('Erro ao enviar dados:', e)
 
 def main():
-    manuals = getManuals()
-    if manuals:
-        document = manuals.get("results", {}).get("document", [])
-        for doc in document:
-            print(doc['fields']['id'])
+        totalPage = 1
+        for page in range(1, totalPage + 1):
+            datas = getAllTuring(page)
+            if datas:
+                queryContext = datas.get("queryContext", {})
+                totalPage = queryContext['pageCount']
+                document = datas.get("results", {}).get("document", [])
+                for doc in document:
+                    print(page + " " + doc['fields']['id'] + " - " + doc['fields']['type'])
 
 if __name__ == '__main__':
     main()
