b7ec728cc5b39d7a9e14a193e796a9915d1a7dd6
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-02 11:06:56 -0300

    test

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 import requests
 import os
 
-API_DESTINO = 'https://api.destino.com/receber'
+CHATVOLT_API_URL = 'https://api.chatvolt.ai/'
+DATASTORE_ID = 'cmbauo40600brx87n8gazln1j'
+API_KEY = '3f157bd4-64ae-4ecb-ac8d-ffe0f89b2149'
 
 HEADERS_DESTINO = {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + self.API_KEY
 }
 
 def getAllTuring(page):
     try:
         resposta = requests.get('https://busca.maplebear.com.br/api/sn/maplebear-prd-publish/search?p='+ str(page) +'&rows=100&_setlocale=pt_BR&nfpr=0&q=', verify=False)
         resposta.raise_for_status()
-        dados = resposta.json()
-        return dados
+        return resposta.json()
     except requests.RequestException as e:
         print('Erro ao buscar dados:', e)
         return None
 
-def enviar_dados(dados):
+def sendToTuring(data):
     try:
-        resposta = requests.post(API_DESTINO, json=dados, headers=HEADERS_DESTINO)
+        data["datastoreId"] = self.DATASTORE_ID
+
+        resposta = requests.post(self.CHATVOLT_API_URL + "datasources", json=data, headers=HEADERS_DESTINO)
         resposta.raise_for_status()
         print('Dados enviados com sucesso:', resposta.status_code)
     except requests.RequestException as e:
         print('Erro ao enviar dados:', e)
 
 def main():
-        for page in range(1, 100):
-            datas = getAllTuring(page)
-            queryContext = datas.get("queryContext", {})
-            if (page > queryContext['pageCount']):
-                break
+    for page in range(1, 100):
+        datas = getAllTuring(page)
+        queryContext = datas.get("queryContext", {})
+        if (page > queryContext['pageCount']):
+            break
 
-            document = datas.get("results", {}).get("document", [])
-            for doc in document:
-                print(str(page) + " " + doc['fields']['id'] + " - " + doc['fields']['mbtype'])
+        document = datas.get("results", {}).get("document", [])
+        for doc in document:
+            print(str(page) + " " + doc['fields']['id'] + " - " + doc['fields']['mbtype'])
+            sendToTuring(doc)
+            break
 
+        break
 
 if __name__ == '__main__':
     main()
