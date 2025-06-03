53a307863cfcc8df2271bc46f41e314185cc9605
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-03 13:27:48 -0300

    test

--- sprinklr_turing.py
+++ sprinklr_turing.py
@@ -7,8 +7,9 @@ def main():
     if login.get('responseCode', False) == 'SUCCESS':
         accessToken = login.get('accessToken', False)
 
-        for page in range(999, 1000):
+        for page in range(0, 100):
             spPosts = sprinklr.getPosts(accessToken, page)
+            print("Page: " + page)
             for spPost in spPosts:
                 print(spPost)
 
