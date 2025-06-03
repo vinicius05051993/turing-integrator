2544343cae9072880d716a38b30133c7132e62db
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-03 20:04:13 -0300

    test

--- ai.py
+++ ai.py
@@ -3,13 +3,13 @@ from transformers import BertModel, BertTokenizer
 
 class Tags:
     def __init__(self):
-        model_name = "neuralmind/bert-base-portuguese-cased"
+        model_name = "paraphrase-multilingual-MiniLM-L12-v2"
         self.model = BertModel.from_pretrained(model_name)
         self.kw_model = KeyBERT(model=self.model)
 
     def get(self, texto, n=8, diversity=0.5):
         return self.kw_model.extract_keywords(
-            texto,
+            texto.lower(),
             keyphrase_ngram_range=(1, 1),
             stop_words='portuguese',
             top_n=n,
