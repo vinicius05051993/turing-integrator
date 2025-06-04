4cfbe3098e8fcf356d5cebf2a79d2a252d5ba0ee
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-04 08:50:48 -0300

    test

--- ai.py
+++ ai.py
@@ -5,10 +5,10 @@ class Tags:
         model_name = "sentence-transformers/distiluse-base-multilingual-cased-v2"
         self.kw_model = KeyBERT(model=model_name)
 
-    def get(self, texto, n=8, diversity=0.3):
+    def get(self, texto, n=8, diversity=0.1):
         return self.kw_model.extract_keywords(
             texto.lower(),
-            keyphrase_ngram_range=(1, 2),
+            keyphrase_ngram_range=(1, 1),
             stop_words=None,
             top_n=n,
             use_mmr=True,
