3db20015a11f8d6246c8a224f4013807988bd6b9
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-05 11:45:50 -0300

    test

--- tools/sprinklr.py
+++ tools/sprinklr.py
@@ -47,7 +47,7 @@ def getPosts(accessToken, page):
         ],
         "page": {
             "page": page,
-            "size": 10
+            "size": 100
         },
         "sorts": [
             {
