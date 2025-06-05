35d6561488cd8fc10d2dbbd2b1cd9e036acc4c12
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-05 17:41:35 -0300

    test

--- sprinklr_turing.py
+++ sprinklr_turing.py
@@ -17,9 +17,6 @@ def main():
                 break
 
             for spPost in spPosts:
-                if spPost['id'] == '6812109789cac71a44c5176a':
-                    continue
-
                 integration = turing.integrationStatus(allManualsTuring, spPost)
 
                 if integration["key"] != None:
