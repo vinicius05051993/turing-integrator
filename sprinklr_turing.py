1824da461f1f851caed1f94381d499c6501be8bd
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-05 14:39:27 -0300

    test

--- sprinklr_turing.py
+++ sprinklr_turing.py
@@ -10,27 +10,28 @@ def main():
 
         allManualsTuring = turing.getAllTuringIds('manual')
 
-        for page in range(0, 100):
-            spPosts = sprinklr.getPosts(accessToken, page)
-
-            if len(spPosts) == 0:
-                break
-
-            for spPost in spPosts:
-                integration = turing.integrationStatus(allManualsTuring, spPost)
-
-                if integration["key"] != None:
-                    allManualsTuring.pop(integration["key"])
-
-                match integration['status']:
-                    case 1:
-                        turing.send(spPost)
-                    case 2:
-                        turing.delete(integration['id'])
-                        turing.send(spPost)
+#         for page in range(0, 100):
+#             spPosts = sprinklr.getPosts(accessToken, page)
+#
+#             if len(spPosts) == 0:
+#                 break
+#
+#             for spPost in spPosts:
+#                 integration = turing.integrationStatus(allManualsTuring, spPost)
+#
+#                 if integration["key"] != None:
+#                     allManualsTuring.pop(integration["key"])
+#
+#                 match integration['status']:
+#                     case 1:
+#                         turing.send(spPost)
+#                     case 2:
+#                         turing.delete(integration['id'])
+#                         turing.send(spPost)
 
         for manualTuringToDelete in allManualsTuring:
             turing.delete(manualTuringToDelete['id'])
+            break
 
 if __name__ == '__main__':
     main()
