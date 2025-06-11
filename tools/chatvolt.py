189311fa8d368920060dbb5a4b55dad72416fb31
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-11 10:15:36 -0300

    test

--- tools/chatvolt.py
+++ tools/chatvolt.py
@@ -32,7 +32,7 @@ def sendPost(docFields : dict, generalAI):
             if contentTags != "":
                 tagsList = contentTags.split('\n')
             else:
-                responseAI = generalAI.get(text, 'Extraia exatamente 8 tags, nenhuma mais e nenhuma menos, que representando o conteúdo do texto acima. As tags devem: ter no máximo 25 caracteres, conter apenas letras (sem números ou símbolos), e estar separadas por vírgula.')
+                responseAI = generalAI.get(text, 'Extraia exatamente 8 tags, nenhuma mais e nenhuma menos, que representando o conteúdo do texto acima. As tags devem: ter no máximo 25 caracteres, conter apenas letras e espaço (sem números ou símbolos), e estar separadas por vírgula.')
                 print(responseAI)
                 tagsList = responseAI.split(',')
 
