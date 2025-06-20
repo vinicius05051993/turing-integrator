1f9efd4a58fee9e74be1e3261ed07d5bd9592afa
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-20 16:29:48 -0300

    test

--- clean_chatvolt.py
+++ clean_chatvolt.py
@@ -5,18 +5,14 @@ def main():
     chatVoltDatas = chatvolt.getAll()
     chatVoltDataSources = chatVoltDatas.get("datasources", {})
 
-    # Agrupa todos os itens por nome
     nomes_agrupados = defaultdict(list)
     for item in chatVoltDataSources:
         nomes_agrupados[item['name']].append(item['id'])
 
-    # Pega apenas os nomes repetidos e seus respectivos IDs
     ids_para_excluir = []
     for nome, ids in nomes_agrupados.items():
         if len(ids) > 1:
             ids_para_excluir.extend(ids[1:])
-
-    print(ids_para_excluir)
 
     for id in ids_para_excluir:
         chatvolt.delete(id)
