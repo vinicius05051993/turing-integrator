832f9a8c02d1bf8bfc2418c05529136a8d220cf5
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-06-20 16:26:45 -0300

    test

--- clean_chatvolt.py
+++ clean_chatvolt.py
@@ -1,16 +1,23 @@
 import tools.chatvolt as chatvolt
-from collections import Counter
+from collections import Counter, defaultdict
 
 def main():
     chatVoltDatas = chatvolt.getAll()
     chatVoltDataSources = chatVoltDatas.get("datasources", {})
 
-    ids = [chatVoltData['name'] for chatVoltData in chatVoltDataSources]
-    contagem = Counter(ids)
+    # Agrupa todos os itens por nome
+    nomes_agrupados = defaultdict(list)
+    for item in chatVoltDataSources:
+        nomes_agrupados[item['name']].append(item['id'])
 
-    resultado = [(id_, qtd) for id_, qtd in contagem.items() if qtd > 1]
+    # Pega apenas os nomes repetidos e seus respectivos IDs
+    ids_para_excluir = []
+    for nome, ids in nomes_agrupados.items():
+        if len(ids) > 1:
+            # Mantém o primeiro e marca os outros para exclusão
+            ids_para_excluir.extend(ids[1:])
 
-    print(resultado)
+    print(ids_para_excluir)
 
 if __name__ == '__main__':
     main()
