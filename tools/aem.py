7c6237000f176a3ba8ef380db5bf5cfa22b12c3a
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-07-29 09:46:03 -0300

    improve

--- tools/aem.py
+++ tools/aem.py
@@ -37,8 +37,8 @@ def find_all_objects(data):
             if "accordionItems" in obj:
                 for accordion in obj['accordionItems']:
                     results.append(accordion['accordionTitle'] + ' ' + remove_html_tags_and_special_chars(accordion['paragraph']))
-#             if "richtext" in obj:
-#                  results.append(remove_html_tags_and_special_chars(obj['richtext']['text']))
+            if "richtext" in obj:
+                 results.append(remove_html_tags_and_special_chars(obj['richtext']['text']))
         elif isinstance(obj, list):
             for item in obj:
                 recursive_search(item)
