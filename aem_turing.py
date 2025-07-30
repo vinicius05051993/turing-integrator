6dff2fe8572a59d8fcf74978ae7d9cbd3f033399
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-07-30 16:28:18 -0300

    improve

--- aem_turing.py
+++ aem_turing.py
@@ -11,6 +11,9 @@ def main():
         if aem.isPost(id):
             proprieties = aem.getContentFragmentProprieties(id)
             pageContent = aem.getPageContent(id)
+            print(pageContent)
+            break
+
             if proprieties and pageContent:
                 contentFragment['lastModified'] = int(time.time() * 1000)
                 integration = aem.integrationStatus(allPostsTuring, contentFragment)
