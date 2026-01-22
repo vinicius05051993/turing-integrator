b666d2d8047e9ebdf9957765ecf39c026791e29c
Vinicius Henrique <viniciush.oliveira@outlook.com>
2026-01-22 14:20:56 -0300

    fix

--- turing_aem.py
+++ turing_aem.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 author_url = "https://author-p120717-e1174076.adobeaemcloud.com"
-credentials = ("oscar.rooij@sebsa.com.br", "adobeNectar321#")
+credentials = ("turing_user", "5DIzbK4@")
 
 dam_folder = "/content/dam/maple-bear/manuais"
 
