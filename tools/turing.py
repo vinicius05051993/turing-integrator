11187fc616b3128d847a6302cdb6db658f6dfd5a
warlleyj <warlleyj.correia@sebsa.com.br>
2026-04-16 17:31:56 -0300

    fix json response treatment

--- tools/turing.py
+++ tools/turing.py
@@ -182,7 +182,11 @@ def send(spPost, mbtype = 'manual'):
     }
 
     response = requests.post(DATA_IN_USE['url_import'], json=data, headers=headers, verify=False)
-    print(mbtype + ' enviada com sucesso:', response.status_code, response.text, response.json(), json.dumps(data))
+    try:
+        response_json = response.json()
+    except ValueError:
+        response_json = None
+    print(mbtype + ' enviada com sucesso:', response.status_code, response.text, response_json, json.dumps(data))
 
     return turingFields
 
